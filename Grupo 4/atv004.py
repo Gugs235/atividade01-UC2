b2c8ab6c76c2b6a241f4265bf237c7088975477e
CelsoLucas <lucascelso23@gmail.com>
2024-10-16 16:44:48 -0400

    commit
    input x, e comentario

--- Grupo 4/atv004.py
+++ Grupo 4/atv004.py
@@ -1,2 +1,4 @@
 while True:
     #o usuario coloca a quantidade de vezes que vai aumentar
+    x = float(input("Digite a quantidade que vai aumentar a experiencia: "))
+    #o usuario coloca a quantidade que normalmente da
